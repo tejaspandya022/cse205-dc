b92fff27f25d217c94c7724b45613e930d50631e
tejaspandya022 <138299426+tejaspandya022@users.noreply.github.com>
2023-09-06 10:15:11 +0530

    Create 231.py

---
+++ assignments/09.08.2023/231.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if (n==0):
+            return False
+        elif (n==1):
+            return True
+        elif (n%2==0):
+            return self.isPowerOfTwo(n/2)
